b0669de1c4d72273ba0ada6e867a12e2754505fd
JuampiTurner <juampiturnerlopez@gmail.com>
2024-07-18 09:49:07 -0300

    ignore it

--- MyPortfolio/src/customHooks/useLanguageHook.jsx
+++ MyPortfolio/src/customHooks/useLanguageHook.jsx
@@ -10,7 +10,5 @@ export const useLanguage = (initialLanguageResources, selectedLanguage) => {
         else return languageResources.ENGLISH[key]
     }
 
-    return {
-        getTranslation
-    }
+    return getTranslation
 }
