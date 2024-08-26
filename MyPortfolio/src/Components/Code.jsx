2a82c745b612695fdb0f132f82340acaadc38cb4
JuampiTurner <juampiturnerlopez@gmail.com>
2024-08-25 23:45:48 -0300

    i just want to sleep

--- MyPortfolio/src/Components/Code.jsx
+++ MyPortfolio/src/Components/Code.jsx
@@ -4,7 +4,7 @@ export const Code = () => {
   return (
     <section id='code' className='code-section'>
       <h2 className='title'>Codigo</h2>
-      <h3>Queria contarte un poco hacerte de este proyecto también...</h3>
+      <h3>Queria contarte un poco de este proyecto también...</h3>
     </section>
   )
 }
--- MyPortfolio/src/Components/Projects.jsx
+++ MyPortfolio/src/Components/Projects.jsx
@@ -1,9 +1,28 @@
 import Section from '../Components/MiniComponents/Section'
+import { useContext } from 'react'
+
+import { useLanguage } from '../customHooks/useLanguageHook'
+import { LanguageContext } from './Context/LanguageContext'
+
+const LanguageResources = {
+  SPANISH: {
+    title: 'Proyectos',
+    text: 'Pronto estaré completando esta sección para que puedas disfrutar un poco de lo que es mi trabajo y a lo que me dedico... '
+  },
+  ENGLISH: {
+    title: 'Projects',
+    text: 'I will soon be completing this section so you can enjoy a bit of what my work is about and what I do... '
+  }
+}
 
 export const Projects = () => {
+
+  const useLanguageContext = useContext(LanguageContext)
+  const { getTranslation } = useLanguage(LanguageResources, useLanguageContext.selectedLanguage)
+
   return (
-    <Section idSection='projects' title='Proyectos'>
-      <p>Projects Section</p>
+    <Section idSection='projects' title={getTranslation('title')}>
+      <p style={{ textAlign: 'center' }}>{getTranslation('text')} 😉🫣</p>
     </Section>
   )
 }
